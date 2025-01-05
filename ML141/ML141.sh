4b31326e67836b16a844c68b9f788fc7d8f790ca
Yeng-Tseng Wang <c00jsw00@gmail.com>
2025-01-06 07:10:29 +0800

    Add files via upload

--- ML141/ML141.sh
+++ ML141/ML141.sh
@@ -1,2 +1,1 @@
-#ML141, Cdc42 GTPase Inhibitor
-obabel -:"COC1C=CC(=CC=1)C1CC(=NN1C1C=CC(=CC=1)S(N)(=O)=O)C1C=CC=CC=1" -opdbqt -O ML141.pdbqt --gen3d --minimize
+obabel -:"COC1C=CC(=CC=1)C1CC(=NN1C1C=CC(=CC=1)S(N)(=O)=O)C1C=CC=CC=1" -opdb -O ML141.pdb --gen3d --minimize
